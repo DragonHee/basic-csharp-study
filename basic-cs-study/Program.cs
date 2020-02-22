ad5df55c064a32464f926a136cced8c0c605e320
kimyonghee <yhk06262@naver.com>
2020-02-22 21:10:33 +0900

    초기 커밋.

---
+++ basic-cs-study/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace basic_cs_study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("hello csharp!");
+            Console.WriteLine("hello csharp!!");
+        }
+    }
+}
